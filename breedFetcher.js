871fec9019292de9a80557d8d99202e16225b21d
Rishad Alam <rishad.s.a@gmail.com>
2023-06-08 03:47:44 -0400

    cat refactored and test cases added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,23 @@
 const request = require("request");
 
-const catInfo = () => {
-  let name = process.argv.slice(2);
-  let url = "https://api.thecatapi.com/v1/breeds/search?q=" + name;
+const fetchBreedDescription = (breedName, callback) => {
+  request(
+    "https://api.thecatapi.com/v1/breeds/search?q=" + breedName,
+    (error, response, body) => {
+      if (error) {
+        //Edge Case: Request Failed
+        callback("Request Failed");
+        return;
+      }
+      const data = JSON.parse(body);
 
-  request(url, (error, response, body) => {
-    if (error) {
-      //Edge Case: Request Failed
-      console.log("Request Failed");
-      //console.log(error);
-
-      process.exit;
-      return;
+      //Edge Case: Breed Not Found
+      if (data.length === 0) {
+        return callback("Breed not found");
+      }
+      callback(null, data[0].description);
     }
-    const data = JSON.parse(body);
-    //Edge Case: Breed Not Found
-    data[0] === undefined
-      ? console.log("Breed not Found")
-      //Print Description
-      : console.log(data[0].description);
-  });
+  );
 };
-catInfo();
+
+module.exports = { fetchBreedDescription };
